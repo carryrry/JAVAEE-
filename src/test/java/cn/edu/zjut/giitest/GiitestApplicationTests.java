20d6b30273078ec3585055332e394ed817538f91
ikunson <2457070563@qq.com>
2024-12-25 16:34:46 +0800

    整合 Spring Initializr 项目到 JAVAEE- 仓库

---
+++ src/test/java/cn/edu/zjut/giitest/GiitestApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zjut.giitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiitestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
