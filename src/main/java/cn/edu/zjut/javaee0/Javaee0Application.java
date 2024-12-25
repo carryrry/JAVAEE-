906e8966aec7b6f66d3459bde6021a1f56beea83
ikunson <2457070563@qq.com>
2024-12-25 13:58:34 +0800

    Init

---
+++ src/main/java/cn/edu/zjut/javaee0/Javaee0Application.java
@@ -0,0 +1,13 @@
+package cn.edu.zjut.javaee0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javaee0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javaee0Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/zjut/javaee0/Javaee0ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zjut.javaee0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javaee0ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
